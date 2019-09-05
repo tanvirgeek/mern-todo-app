7df02159c16de87bf64ac3f446cfecb14bd72f89
tanvirgeek <tanvirgeek@gmail.comm>
2019-09-05 16:18:53 +0600

    this is a mern to do appp first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodoList from "./components/todo-list.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<Router>
+		<div className="container">
+			<h2>MERN Stack To Do App</h2>
+			<Route path="/" exact component={TodoList}/> 
+			<Route path="/edit/:id" component = {EditTodo} />
+			<Route path= "/create" component = {CreateTodo} />
+		</div>
+	</Router>
+    
   );
 }
 
---
+++ src/components/create-todo.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class CreateTodo extends Component{
+	render(){
+		return (
+			<div>
+				<p>Welcome to create list component</p>
+			</div>
+		)
+	}
+}
