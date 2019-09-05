18a51ea3740d618e997fb6f5bf7f38cfa4b51998
tanvirgeek <tanvirgeek@gmail.comm>
2019-09-05 22:38:28 +0600

    Night time work sep 5

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodoList from "./components/todo-list.component";
+import logo from "./logo.jpg";
 
 function App() {
   return (
 	<Router>
 		<div className="container">
+			<nav className = "navbar navbar-expand-lg navbar-light bg-light">
+				<a className="navbar-brand" href="http://www.google.com" target="_blank">
+					<img src={logo} width="30" height = "30" alt="tanvirgeekApps"/>
+				</a>
+				<Link to="/" className="navbar-brand"> MERN-Stack Todo App</Link>
+				<div className="collaspe nav-collaspe">
+					<ul className="navbar-nav mr-auto">
+						<li classname="navbar-item">
+							<Link to="/" className="nav-link">Todos</Link>
+						</li>
+						<li classname="navbar-item">
+							<Link to="/create" className="nav-link">Create Todos</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
 			<h2>MERN Stack To Do App</h2>
 			<Route path="/" exact component={TodoList}/> 
 			<Route path="/edit/:id" component = {EditTodo} />
